9521fa6495efe4ce5dea9f84cbb54e0e031f8f57
Tarunna <tarunnasen@gmail.com>
2026-01-03 19:58:54 +0600

    Image responsiveness imporved

--- src/Components/HumanCard.jsx
+++ src/Components/HumanCard.jsx
@@ -1,7 +1,7 @@
 export default function HumanCard({ img, alt, name, role, description }) {
   return (
     <div className="reveal flex items-center justify-center gap-6 p-5 border-2 border-black/10 rounded-lg bg-white drop-shadow-xl cursor-pointer transition duration-200 ease-in-out hover:-translate-y-1 hover:scale-105">
-      <img src={img} className="h-12 md:h-20" alt={alt} />
+      <img src={img} className="h-20" alt={alt} />
       <div className="flex flex-col items-start">
         <p className="text-base md:text-xl font-bold">{name}</p>
         <p className="text-xs md:sm">
