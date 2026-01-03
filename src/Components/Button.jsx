1e39ff59b069a5c8b006d4cadcf53a0d9e641217
Tarunna <tarunnasen@gmail.com>
2026-01-03 19:15:50 +0600

    All logic implemented

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,7 +1,7 @@
-export default function Button({ id, text }) {
+export default function Button({ text, onClick }) {
   return (
     <button
-      id={id}
+      onClick={onClick}
       class="w-full bg-[#30C7B8] text-white font-bold py-3 sm:py-4 px-6 rounded-lg text-lg sm:text-xl rounded-lg cursor-pointer drop-shadow-xl transition duration-200 ease-in-out hover:-translate-y-1 hover:scale-105"
     >
       {text}
--- src/Pages/Track.jsx
+++ src/Pages/Track.jsx
@@ -11,35 +11,130 @@ export default function Track() {
   const [time, setTime] = useState("");
   const [result, setResult] = useState(false);
   const [showList, setShowList] = useState(false);
+  const [resultTitle, setResultTitle] = useState("");
+  const [resultSubTitle, setResultSubTitle] = useState("");
+  const [borderColor, setBorderColor] = useState("");
   const suggestions = useMemo(() => {
     if (!query.trim()) return [];
 
     const q = query.toLowerCase();
 
     return Object.entries(stopMap).filter(
-      ([bn, en]) => bn.includes(query) || en.includes(q)
+      ([bn, en]) => bn.includes(query) || en.toLowerCase().includes(q)
     );
   }, [query]);
+
+  const tracking = () => {
+    if (!query.trim() || !time) {
+      setResult(true);
+      setResultTitle("সঠিকভাবে স্টপ এবং সময় সিলেক্ট করুন।");
+      setResultSubTitle("সার্চ করার সময় অবশ্যই সময় এবং স্টপ দুটোই দিতে হবে।");
+      setBorderColor("border-red-600");
+      setQuery("");
+      setTime("");
+      return;
+    }
+
+    const exists = Object.entries(stopMap).some(
+      ([bn, en]) => bn === query || en.toLowerCase() === query.toLowerCase()
+    );
+
+    if (!exists) {
+      setResult(true);
+      setResultTitle("ভুল স্টপ");
+      setResultSubTitle(
+        "আপনার সিলেক্ট করা স্টপটি পাওয়া যায়নি। সাজেশন থেকে সঠিক স্টপটি সিলেক্ট করুন।"
+      );
+      setBorderColor("border-red-600");
+      setQuery("");
+      setTime("");
+      return;
+    }
+
+    const passed = Object.values(mockData).some((bus) =>
+      bus.passedStops.includes(query)
+    );
+
+    if (passed) {
+      setResult(true);
+      setResultTitle("দুঃখিত!");
+      setResultSubTitle(
+        "ক্যাম্পাস বাস আপনার স্টপ ইতিমধ্যে পার করে চলে গিয়েছে।"
+      );
+      setBorderColor("border-red-600");
+      setQuery("");
+      setTime("");
+      return;
+    }
+
+    const bus = Object.values(mockData).find((b) =>
+      b.nextStops.includes(query)
+    );
+    const stopIndex = bus.nextStops.indexOf(query);
+
+    const minutesNeeded = bus.requiredTimes
+      .slice(0, stopIndex + 1)
+      .reduce((sum, t) => sum + t, 0);
+
+    const [h, m] = time.split(":").map(Number);
+    const base = new Date();
+    base.setHours(h, m, 0);
+    base.setMinutes(base.getMinutes() + minutesNeeded);
+
+    const arrivalTime = base
+      .toLocaleTimeString("bn-BD", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true,
+      })
+      .replace(/AM|PM|পূর্বাহ্ণ|অপরাহ্ণ/g, "")
+      .trim();
+
+    if (bus) {
+      setResult(true);
+      setResultTitle(`মাত্র ${minutesNeeded.toLocaleString("bn-BD")} মিনিট!`);
+      setResultSubTitle(
+        `ক্যাম্পাস বাস আপনার স্টপ ${query} এ পৌঁছাবে আনুমানিক ${arrivalTime} এ`
+      );
+      setBorderColor("border-green-400");
+      setQuery("");
+      setTime("");
+      return;
+    } else {
+      setResult(true);
+      setResultTitle("কোনো বাস নেই");
+      setResultSubTitle("আজকে আপনার স্টপগামী আর কোন ক্যাম্পাস বাস আপাতত নেই। ");
+      setBorderColor("border-red-600");
+      setQuery("");
+      setTime("");
+      return;
+    }
+  };
 
   return (
     <>
       <div className="flex flex-col min-h-screen">
         <Header />
-        <main class="grow pb-8 px-10">
+        <main className="grow pb-8 px-10">
           <Heading
             text="বাস "
             maintext="ট্র্যাকিং"
             subtext="সকল বাস ছাড়া সময় সকাল ৭টা এবং দুপুর ১২টা। ফিরতি বাস ছাড়ার সময় দুপুর ১:৩০
         এবং বিকাল ৬টা"
           />
-          <section class="flex flex-col items-center justify-center gap-6 pb-10">
-            {result && (<div
-              id="resultInfoDiv"
-              class="p-6 border-2 rounded-lg bg-white drop-shadow-xl cursor-pointer transition duration-200 ease-in-out hover:-translate-y-1 hover:scale-105"
-            ></div>)}
+          <section className="flex flex-col items-center justify-center gap-6 pb-10">
+            {result && (
+              <div
+                className={`p-6 border-2 ${borderColor} rounded-lg bg-white drop-shadow-xl cursor-pointer transition duration-200 ease-in-out hover:-translate-y-1 hover:scale-110`}
+              >
+                <p className="text-2xl font-bold">{resultTitle}</p>
+                <p className="pt-2 text-base">{resultSubTitle}</p>
+              </div>
+            )}
+
             <div
               id="search"
-              class="flex flex-col justify-center gap-4 sm:gap-6"
+              className="flex flex-col justify-center gap-4 sm:gap-6"
             >
               <div>
                 <InputField
@@ -56,7 +151,7 @@ export default function Track() {
                 {showList && suggestions.length > 0 && (
                   <div
                     id="result-div"
-                    class="p-4 absolute z-10 bg-white border border-gray-300 mt-1 rounded-xl shadow-lg max-h-60 overflow-y-auto"
+                    className="p-4 absolute z-10 bg-white border border-gray-300 mt-1 rounded-xl shadow-lg max-h-60 overflow-y-auto"
                   >
                     <ul id="result">
                       {suggestions.map(([bn, en]) => (
@@ -83,8 +178,8 @@ export default function Track() {
                 value={time}
                 onChange={setTime}
               />
-              <div class="reveal text-center mt-6">
-                <Button id="searchButton" text="সার্চ করুন" />
+              <div className="reveal text-center mt-6">
+                <Button text="সার্চ করুন" onClick={tracking} />
               </div>
             </div>
           </section>
